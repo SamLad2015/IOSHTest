8478033d2819ea195ba335fa5186eb17670aa0d4
sam_at_NT <sangram.lad@nationaltrust.org.uk>
2019-10-01 23:13:39 +0100

    implemented Joi validation

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const Joi = require('joi');
 const cors = require('cors');
 const port = 3000;
 
+const userValidationSchema = Joi.object().keys({
+  id: Joi.number().integer().min(1)
+});
+
 app.use(cors());
 
 app.get('/api/users', (req, res) => {
@@ -18,14 +22,19 @@ app.get('/api/users', (req, res) => {
 });
 
 app.get('/api/users/:id', (req, res) => {
-  const apiUrl = 'https://jsonplaceholder.typicode.com/users?id=' + req.params.id;
-  api_helper.make_API_call(apiUrl)
-    .then(response => {
-      res.json(response)
-    })
-    .catch(error => {
-      res.send(error)
-    })
+  const { error } = Joi.validate(req.params, userValidationSchema);
+  if (error) {
+    res.send(error);
+  } else {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users?id=' + req.params.id;
+    api_helper.make_API_call(apiUrl)
+      .then(response => {
+        res.json(response)
+      })
+      .catch(error => {
+        res.send(error)
+      });
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
